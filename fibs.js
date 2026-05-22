7af892e481a749e3c881fdfec9458bf6cc6a7d22
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-05-22 10:55:38 +0200

    feat: add iterative fibonacci function

---
+++ fibs.js
@@ -0,0 +1,17 @@
+function fibs(number) {
+  const sequence = [];
+
+  for (let i = 0; i < number; i++) {
+    if (i === 0) {
+      sequence.push(0);
+    } else if (i === 1) {
+      sequence.push(1);
+    } else {
+      sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+  }
+
+  return sequence;
+}
+
+console.log(fibs(8));
